028725d8e7e27ad144bfd8d31a62e51cd6e1bf09
Cássia <cascrisbern@gmail.com>
2024-02-06 17:12:16 +0200

    added styles for the Home component added content for the HOme component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I am going to work on</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount">during</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play />
+          Start
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
